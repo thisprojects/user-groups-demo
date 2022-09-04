bf2aad16793ba40f7c968168055a7723617368c1
Nathan Downes <p2677720@my365.dmu.ac.uk>
2022-09-04 12:10:01 +0100

    first commit project template

---
+++ src/main/java/com/usergroups/User/Groups/Demo/UserGroupsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.usergroups.User.Groups.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserGroupsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserGroupsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usergroups/User/Groups/Demo/UserGroupsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usergroups.User.Groups.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserGroupsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
